217c5a114028e893f763652c949f7911112f327f
cyxae <cyxae@amphitryon.nrst.fr>
2025-07-25 10:29:47 -0400

    cyxae-keymap: add mirror right hand

--- keyboards/hazel/bad_wings_v2/keymaps/cyxae/keymap.c
+++ keyboards/hazel/bad_wings_v2/keymaps/cyxae/keymap.c
@@ -14,6 +14,7 @@
 #define HRM_SCLN  RGUI_T(KC_SCLN)
 #define CKC_BSPC  LT(L_NAV, KC_BSPC)
 #define CKC_SPC   LT(L_NUM, KC_SPC)
+#define CKC_MIR   OSL(L_MIR)
 #define ALT_F4    LALT(KC_F4)
 
 #define CIRQUE_CPI 2000
@@ -22,6 +23,7 @@ enum layers {
   L_ALPHA,
   L_NUM,
   L_NAV,
+  L_MIR,
   LAYER_LENGTH
 };
 
@@ -112,14 +114,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NUM] = LAYOUT_split_3x5_3(
     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,            KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,
     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
-    KC_F11,     KC_F12,     KC_NO,      KC_SPC,     KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_KP_DOT,
-                            KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,    KC_NO,      KC_NO
+    KC_F11,     KC_F12,     KC_NO,      KC_SPC,     KC_NO,            KC_NO,      KC_NO,      KC_TRNS,    KC_NO,      KC_KP_DOT,
+                            KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,    KC_TRNS,    KC_NO
   ),
   [L_NAV] = LAYOUT_split_3x5_3(
     KC_WBAK,    ALT_TAB,    C(KC_N),    KC_WFWD,    ALT_F4,           S(KC_INS),  KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,
-    KC_ESC,     KC_ENT,     KC_TAB,     S(KC_TAB),  KC_DEL,           KC_NO,      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
-    KC_CAPS,    KC_DOWN,    KC_UP,      KC_SPC,     KC_BSPC,          KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,
-                            KC_NO,      KC_NO,      KC_TRNS,          KC_TRNS,    KC_NO,      KC_NO
+    KC_ESC,     KC_ENT,     KC_TAB,     S(KC_TAB),  KC_DEL,           KC_CAPS,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
+    KC_DOWN,    CKC_MIR,    KC_UP,      KC_SPC,     KC_BSPC,          KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,
+                            KC_NO,      KC_TRNS,    KC_TRNS,          KC_TRNS,    KC_NO,      KC_NO
+  ),
+  [L_MIR] = LAYOUT_split_3x5_3(
+    KC_P,       KC_O,       KC_I,       KC_U,       KC_Y,             KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+    HRM_SCLN,   HRM_L,      HRM_K,      HRM_J,      KC_H,             KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+    KC_SLSH,    KC_DOT,     KC_COMM,    KC_M,       KC_N,             KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+                            KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_NO,      KC_NO,      KC_NO
   )
 };
 
