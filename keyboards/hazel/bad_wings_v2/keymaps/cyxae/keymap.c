8e189a6621121f236dffe808c1cb9ef6e9bae575
cyxae <cyxae@amphitryon.nrst.fr>
2025-10-17 13:06:22 -0400

    cyxae-keymap: hummingbird with combos

--- keyboards/hazel/bad_wings_v2/keymaps/cyxae/keymap.c
+++ keyboards/hazel/bad_wings_v2/keymaps/cyxae/keymap.c
@@ -93,10 +93,18 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 }
 
 // NumRow 1 + Clic gauche = Download mode to allow upgrade firmware with qmk flash
-const uint16_t PROGMEM dfu_cmb[] = {KC_1, MS_BTN1, COMBO_END};
+const uint16_t PROGMEM dfu_cmb[]  = {KC_1, MS_BTN1, COMBO_END};
+const uint16_t PROGMEM hmb_z[]    = {KC_W, HRM_S, COMBO_END};
+const uint16_t PROGMEM hmb_b[]    = {KC_R, HRM_F, COMBO_END};
+const uint16_t PROGMEM hmb_n[]    = {KC_U, HRM_J, COMBO_END};
+const uint16_t PROGMEM hmb_slsh[] = {KC_O, HRM_L, COMBO_END};
 
 combo_t key_combos[] = {
   COMBO(dfu_cmb, QK_BOOTLOADER),
+  COMBO(hmb_z, KC_Z),
+  COMBO(hmb_b, KC_B),
+  COMBO(hmb_n, KC_N),
+  COMBO(hmb_slsh, KC_SLSH),
 };
 
 // Ctrl + PgDn = Ctrl + PgUp
@@ -106,19 +114,23 @@ const key_override_t ctrl_pgdn_override = ko_make_basic(MOD_MASK_CTRL, KC_PGDN, 
 const key_override_t ctrl_pgup_override = ko_make_basic(MOD_MASK_CTRL, KC_PGUP, C(KC_PGDN));
 
 // Left Ctrl + G = Ctrl + A
-const key_override_t ctrl_g_override = ko_make_basic(MOD_BIT(KC_LCTL), KC_G, C(KC_SCLN));
+const key_override_t lctrl_g_override = ko_make_basic(MOD_BIT(KC_LCTL), KC_G, C(KC_SCLN));
+
+// Left Ctrl + B = Ctrl + X
+const key_override_t lctrl_b_override = ko_make_basic(MOD_BIT(KC_LCTL), KC_T, C(KC_P));
 
 const key_override_t *key_overrides[] = {
   &ctrl_pgdn_override,
   &ctrl_pgup_override,
-  &ctrl_g_override,
+  &lctrl_g_override,
+  &lctrl_b_override,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_ALPHA] = LAYOUT_split_3x5_3(
     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
     HRM_A,      HRM_S,      HRM_D,      HRM_F,      KC_G,             KC_H,       HRM_J,      HRM_K,      HRM_L,      HRM_SCLN,
-    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,
+    KC_NO,      KC_X,       KC_C,       KC_V,       KC_NO,            KC_NO,      KC_M,       KC_COMM,    KC_DOT,     KC_NO,
                             KC_LSFT,    CKC_BSPC,   MS_BTN1,          MS_BTN2,    CKC_SPC,    KC_RALT
   ),
   [L_NUM] = LAYOUT_split_3x5_3(
@@ -128,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,    KC_TRNS,    KC_NO
   ),
   [L_NAV] = LAYOUT_split_3x5_3(
-    KC_WBAK,    ALT_TAB,    C(KC_N),    KC_WFWD,    ALT_F4,           S(KC_INS),  KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,
+    KC_WBAK,    ALT_TAB,    C(KC_SLSH), KC_WFWD,    ALT_F4,           S(KC_INS),  KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,
     KC_ESC,     KC_ENT,     KC_TAB,     S(KC_TAB),  KC_DEL,           KC_CAPS,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
     KC_DOWN,    CKC_MIR,    KC_UP,      KC_SPC,     KC_BSPC,          KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,
                             KC_NO,      KC_TRNS,    KC_TRNS,          KC_TRNS,    KC_NBSPC,   KC_NO
